1998cde36efef5722cdf0d790349c1f2997bce57
kamal chibrani <kamalchibrani@kamals-MacBook-Air.local>
2018-10-25 18:53:13 +0530

    [comint]

---
+++ Podfile
@@ -0,0 +1,8 @@
+ platform :ios, '9.0'
+
+target 'toDO2' do
+  use_frameworks!
+
+  # Pods for toDO2
+    pod 'RealmSwift'
+end
---
+++ toDO2/Data Model/Category.swift
@@ -0,0 +1,16 @@
+//
+//  Category.swift
+//  toDO2
+//
+//  Created by kamal chibrani on 13/10/18.
+//  Copyright © 2018 kamal chibrani. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Category: Object {
+    @objc dynamic var name : String = ""
+}
+
+let items = List<Item>()
