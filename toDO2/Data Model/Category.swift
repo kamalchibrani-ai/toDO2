0361de89d817d156e91d6f372b2a6a406a53b9f5
Kamal <kamalchibrani@kamals-macbook-air.local>
2018-10-25 18:59:31 +0530

    nothing imp

--- toDO2/Data Model/Category.swift
+++ toDO2/Data Model/Category.swift
@@ -11,6 +11,7 @@ import RealmSwift
 
 class Category: Object {
     @objc dynamic var name : String = ""
+    let items = List<Item>()
 }
 
-let items = List<Item>()
+
--- toDO2/Data Model/Item.swift
+++ toDO2/Data Model/Item.swift
@@ -9,10 +9,12 @@
 import Foundation
 import RealmSwift
 
-class Item: Object {
+class Item : Object {
     @objc dynamic var title : String = ""
     @objc dynamic var done : Bool = false
+    @objc dynamic var dateCreated : Date!
+    let parentCategory = LinkingObjects(fromType: Category.self, property: "items")
 }
 
 
-let parentCategory = LinkingObjects(fromType: Category.self, property: "items")
+
